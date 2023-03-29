7d8418c91108a65671cdd950a0d43ae0ba7f4409
Garry <128676726+itzgarry@users.noreply.github.com>
2023-03-29 22:10:50 +0530

    react component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./main.css";
 
 function PrintSomething() {
-  return <h1>Garry</h1>;
+  return (
+    <>
+      <div className="body">
+        <div className="container">
+          <h3>Learn Coding Free, With Us...</h3>
+          <h1>Learn With Garry</h1>
+        </div>
+        <div className="sec_1_container">
+          <div className="sec_1_content_container">
+            <h3>Learn Flutter Free , With Us</h3>
+            <h1>IT'S TIME TO LEARN FLUTTER FOR FREE WITH US</h1>
+            <span className="divider"></span>
+            <p>
+              “Any fool can write code that a computer can understand. Good
+              programmers write code that humans can understand.”
+            </p>
+            <div className="btn_content_sec">
+              <a className="btn_1" href="#">
+                LET'S GO
+              </a>
+              <a className="btn_2" href="#">
+                ABOUT US
+              </a>
+            </div>
+          </div>
+          <div className="sec_1_image_container">
+            {/* <img src="/images/learn_flutter.png" alt="Flutter"> */}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
