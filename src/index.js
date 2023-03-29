92caefaf46b1d668cc531f2e666fe59ea2b00834
Garry <128676726+itzgarry@users.noreply.github.com>
2023-03-29 22:37:42 +0530

    adding image

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./main.css";
+import myImage from "./images/img.jpg";
 
 function PrintSomething() {
   return (
     <>
       <div className="body">
         <div className="container">
-          <h3>Learn Coding Free, With Us...</h3>
+          <h3>Learn Coding...</h3>
           <h1>Learn With Garry</h1>
         </div>
         <div className="sec_1_container">
@@ -29,7 +30,7 @@ function PrintSomething() {
             </div>
           </div>
           <div className="sec_1_image_container">
-            {/* <img src="/images/learn_flutter.png" alt="Flutter"> */}
+            <img class="sec_1_image" src={myImage} alt="My Image" />
           </div>
         </div>
       </div>
