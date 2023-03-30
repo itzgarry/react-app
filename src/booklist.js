db173d19869a98137425f713fa3f0460811bd790
Garry <128676726+itzgarry@users.noreply.github.com>
2023-03-30 15:28:49 +0530

    jsx css and jsx javascript

--- src/booklist.js
+++ src/booklist.js
@@ -17,30 +17,22 @@ const BookList = () => {
 };
 
 const Book = () => {
+  const title = "Learn Flutter";
+  const description = "Coding is life!";
+  const inlineStyle = {
+    color: "#617d98",
+  };
   return (
     <article className="book">
-      <Image></Image>
-      <Title></Title>
-      <Discription></Discription>
+      <img
+        className="img-fluid rounded mx-auto d-block"
+        src={flutterBird}
+        alt="FlutterBird"
+      />
+      <h2>{title}</h2>
+      <h4 style={inlineStyle}>{description}</h4>
     </article>
   );
-};
-
-const Image = () => (
-  <img
-    className="img-fluid rounded mx-auto d-block"
-    src={flutterBird}
-    alt="FlutterBird"
-  />
-);
-
-const inlineStyle = {
-  color: "#617d98",
-};
-
-const Title = () => <h2>Learn Flutter</h2>;
-const Discription = () => {
-  return <h4 style={inlineStyle}>Coding is life!</h4>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
